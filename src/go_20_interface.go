7f185dec8219c4293effec7b73f07ec19d36096a
pengzh <pengzh@asiainfo.com>
2019-08-24 16:38:23 +0800

    接口

---
+++ src/go_20_interface.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//几何接口
+type geometry interface {
+	area() float64
+	perimeter() float64
+}
+type rectangle struct {
+	width  float64
+	height float64
+}
+type circle struct {
+	radius float64
+}
+
+//实现接口
+func (r rectangle) area() float64 {
+	return r.width * r.height
+}
+func (r rectangle) perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+//展示方法
+func measure(geometry geometry) {
+	fmt.Println(geometry)
+	fmt.Println("area S=", geometry.area())
+	fmt.Println("length L=", geometry.perimeter())
+}
+
+func main() {
+	r := circle{radius: 9}
+	p := rectangle{
+		width:  12,
+		height: 3.2,
+	}
+	measure(r)
+	measure(p)
+
+}
