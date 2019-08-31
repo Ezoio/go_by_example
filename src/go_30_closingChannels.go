4a6815996ffd43b2e8b29829e10ca5964eaf4c71
pengzh <pengzh@asiainfo.com>
2019-08-31 23:37:22 +0800

    通道关闭              close channel

--- src/go_30_closingChannels.go
+++ src/go_30_closingChannels.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 )
+
 /**
 *@Description:
 关闭 一个通道意味着不能再向这个通道发送值了。
@@ -13,25 +14,30 @@ import (
 */
 func main() {
 
-	jobs := make(chan int ,5)
+	jobs := make(chan int, 5)
 	done := make(chan bool)
 
 	go func() {
-		for j,more := <-jobs {
+		for {
+			j, more := <-jobs //通道关闭后 more = false
 			if more {
-				fmt.Println(" received message " + j,"more = "+more )
-			}else{
-				fmt.Println(" all jobs done " +j, "more = " +more)
+				fmt.Println(" received j=", j, "more = ", more)
+			} else {
+				fmt.Println(" all jobs done ", j, "more = ", more)
+				done <- true
+				return
 			}
 		}
 	}()
 
-	for i:=3;i>0;i--{
-		jobs<-i
+	for i := 3; i > 0; i-- {
+		jobs <- i
+		fmt.Println("  jobs send ", i)
 		time.Sleep(time.Second)
+		fmt.Println(done)
 	}
 	fmt.Println(" all  jobs send ")
 	close(jobs)
-
-	<- done
+	fmt.Println(done)
+	<-done
 }
