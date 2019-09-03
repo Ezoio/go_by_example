8760dfd1c6e5a49a853f0b8ef5a2e0fbc0887b92
pengzh <pengzh@asiainfo.com>
2019-09-03 20:11:43 +0800

    排序 sort

---
+++ src/go_39_sort.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/**
+*@Description:
+Go 的 sort 包实现了内置和用户自定义数据类型的排序功能。我们首先关注内置数据类型的排序
+*@Author: imi
+*@date: 2019/9/3
+*/
+func main() {
+
+	str := []string{"f", "m", "h", "z", "p"}
+	sort.Strings(str)
+	fmt.Println(str)
+
+}
