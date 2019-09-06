e56002bc4609842ea0999fdf2322237f6265f0ef
pengzh <pengzh@asiainfo.com>
2019-09-06 16:04:27 +0800

    组合函数 collection function

--- src/go_43_collectionFunctions.go
+++ src/go_43_collectionFunctions.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 /**
 *@Description:
 我们经常需要程序在数据集上执行操作，比如选择满足给定条件的所有项，
@@ -22,6 +27,40 @@ func includeIndexStr(str []string, s string) bool {
 	return indexStr(str, s) > 0
 }
 
-func main() {
+func any(str []string, f func(string) bool) bool {
 
+	for _, v := range str {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func all(str []string, f func(string) bool) bool {
+
+	for _, v := range str {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func filter(str []string, f func(string) bool) []string {
+	ss := make([]string, 0)
+	for _, v := range str {
+		if f(v) {
+			ss = append(ss, v)
+		}
+	}
+	return ss
+}
+func main() {
+	str := []string{"apple", "peach", "cherry", "orange", "potato"}
+	fmt.Println(indexStr(str, "orange"))
+	fmt.Println(includeIndexStr(str, "apple"))
+	fmt.Println(filter(str, func(s string) bool {
+		return strings.Contains(s, "e")
+	}))
 }
